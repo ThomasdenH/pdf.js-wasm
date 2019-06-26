532119ecd911a45c765b6ad58c11b1c3e0afc448
Thomas <denhollander.thomas@gmail.com>
2019-06-26 23:21:00 +0200

    md5

--- src/core/crypto.rs
+++ src/core/crypto.rs
@@ -1,6 +1,8 @@
 use md5;
 
 #[cfg(target_arch = "wasm32")]
+use js_sys::Uint8Array;
+#[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
 
 mod arc_four_cipher;
@@ -11,7 +13,12 @@ pub fn calculate_md5(data: &[u8], offset: usize, length: usize) -> [u8; 16] {
     md5::compute(&data[offset..(offset + length)]).into()
 }
 
-#[cfg_attr(target_arch = "wasm32", wasm_bindgen(js_name = calculateMD5))]
-pub fn calculate_md5_wasm(data: &[u8], offset: usize, length: usize) -> Vec<u8> {
-    calculate_md5(dbg!(data), offset, length).to_vec()
+#[cfg(target_arch = "wasm32")]
+#[wasm_bindgen(js_name = calculateMD5)]
+pub fn calculate_md5_wasm(data: Uint8Array, offset: usize, length: usize) -> Uint8Array {
+    let mut data_vec = Vec::with_capacity(data.length() as usize);
+    data.copy_to(&mut data_vec);
+    let slice = calculate_md5(&data_vec, offset, length).to_vec();
+    // TODO: Use from when it is available
+    unsafe { Uint8Array::new(&Uint8Array::view(&slice)) }
 }
